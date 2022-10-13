c365e3b65d789b8256be950fd8c2bfd8c4a525af
Adam Lelito <adam.lelito@microsoft.wsei.edu.pl>
2022-10-13 09:42:27 +0200

    lab2_finish

---
+++ lab2/main.js
@@ -0,0 +1,37 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+var id = null;
+function startMoving () {
+    var elem = document.getElementsByClassName("mySlides fade");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            poss++;
+            elem.style.left = pos + 'px'; 
+        }
+    }
+}
